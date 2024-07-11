8a17cb7128457adfa7d484ffd67849912d83b18d
Ashish Pandey <ashishjeevanlal123@gmail.com>
2024-07-12 00:43:21 +0530

    comit first time project in git

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -14,7 +14,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     Timer(Duration(seconds: 3), () {
       Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(),));
